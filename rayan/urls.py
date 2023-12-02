ea0aa1840f2465ddbc4cb93e32ed0a83a06869d0
Wissam <kw_aissaoui@esi.dz>
2023-12-02 22:45:04 +0100

    fix errors

--- rayan/urls.py
+++ rayan/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from . import views
+
+app_name = 'tasks'
+
 urlpatterns = [
     path('rayan', views.index, name='index'),
     path('add_task', views.add, name='add')
--- rayan/views.py
+++ rayan/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django import forms
 
 from django.http import HttpResponse
@@ -6,7 +6,7 @@ from django.urls import reverse
 
 class new_form(forms.Form):
     task=forms.CharField(label='new_task')
-    priority=forms.IntegerField(label="priotity", min_value=1, max_value=10)
+    priority=forms.IntegerField(label="priotity", min_value=1, max_value=6)
     
 tasks=['foo', 'bar', 'baz']
 
@@ -23,8 +23,10 @@ def add(request):
         if form.is_valid():
             task=form.cleaned_data["task"]
             tasks.append(task)
-            return HttpResponse(reverse("tasks:index"))
+            # return HttpResponse(reverse('tasks:index'))
+            return redirect('tasks:index') 
         else:
+            print("Form is not valid!")
             return render(request, 'hello/index2.html')
     return render(request, 'hello/index2.html', {
     "form":new_form()
