dcc36d8fdfaee9e2aae1686b3e2edec0f06e0929
rayan <mohemedryn@gamil.com>
2023-12-02 21:14:04 +0100

    first commit

---
+++ rayan/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+    path('rayan', views.index, name='index'),
+    path('add_task', views.add, name='add')
+]
+
---
+++ rayan/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+from django import forms
+
+from django.http import HttpResponse
+from django.urls import reverse
+
+class new_form(forms.Form):
+    task=forms.CharField(label='new_task')
+    priority=forms.IntegerField(label="priotity", min_value=1, max_value=10)
+    
+tasks=['foo', 'bar', 'baz']
+
+
+def index(request):
+    return render(request, 'hello/index.html', {
+        'tasks':tasks
+    })
+    
+    
+def add(request):
+    if request.method=="POST":
+        form=new_form(request.POST)
+        if form.is_valid():
+            task=form.cleaned_data["task"]
+            tasks.append(task)
+            return HttpResponse(reverse("tasks:index"))
+        else:
+            return render(request, 'hello/index2.html')
+    return render(request, 'hello/index2.html', {
+    "form":new_form()
+    })
